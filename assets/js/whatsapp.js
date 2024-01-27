46c0ba9bfecb04763a65651ec934c968d7eed75a
Hendras <hendzcode@gmail.com>
2024-01-28 05:44:39 +0700

    :sparkles: Adding new feature whatsapp

---
+++ assets/js/whatsapp.js
@@ -0,0 +1,44 @@
+$(document).ready(() => {
+    askingUnit();
+});
+
+const askingUnit = () => {
+    const productList = [
+        'AVANZA',
+        'Innova E',
+        'Innova Reborn',
+        'Innova Zenix Hybrid',
+        'Alphard',
+        'Alphard Transformer',
+        'Mercedes E Class'
+    ];
+
+    const whatsappNumber = '';
+
+    $('.product-action .btn-primary').each((index, obj) => {
+        var index = index;
+
+        $(obj).on('click', (e) => {
+            e.preventDefault();
+
+            // Aborting if number didn't exist
+            if (!whatsappNumber) {
+                console.log('Set the number first.');
+                return
+            };
+
+            const message = setMessage(productList[index]);
+
+            const url = 'https://wa.me/' 
+                + whatsappNumber 
+                + '?text=' 
+                + message;
+
+            window.open(url);
+        })
+    })
+}
+
+const setMessage = (unit) => {
+    return 'Hai, apakah ' + unit + ' tersedia ? terimakasih.';
+}
